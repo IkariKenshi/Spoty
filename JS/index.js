e993913ec4950d6c57d27bec7d4f74d63fc3a8a1
Arath Morales <arath020310@gmail.com>
2023-03-09 09:40:16 -0600

    Avance

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,7 @@
 const cardTop = document.querySelector('#cardTop').content
 const contenido = document.querySelector('#contenido')
 const fragment = document.createDocumentFragment()
+const btnBuscar = document.getElementById('buscador')
 let topHundred = [];
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -31,11 +32,66 @@ const creaCards = () => {
     topHundred.forEach((song) => {
         cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
         cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+        let artists = ''
+        let size = song.trackMetadata.artists.length
+        song.trackMetadata.artists.forEach((item, index) =>{
+            
+            if(index === size - 1){ 
+            artists += item.name
+        }
+            else{
+                artists += item.name + ' / '
+            } 
+        })
+        cardTop.querySelector('.artistname').textContent = artists
+
         const clone = cardTop.cloneNode(true)
         fragment.appendChild(clone)
     });
     contenido.appendChild(fragment)
 }
 
+/*btnBuscar.addEventListener('keyup', () => {
+    console.log('Tecla: ',btnBuscar.value)
+    var array = []
+    for(const obj of topHundred){
+        array.push(obj.trackMetadata.trackName)
+    }
+
+    let filtrado = array.includes(btnBuscar.value.toLowerCase() )
+    //const BuscarCancion = topHundred.trackMetadata.filter((cancion) = cancion.trackName == btnBuscar.value)
+    
+    console.log('Prueba', filtrado)
+})*/
 
 
+btnBuscar.addEventListener('keyup', () => {
+    const searchTerm = btnBuscar.value.toLowerCase()
+    const filtroCancion = topHundred.filter((song) => {
+      return song.trackMetadata.trackName.toLowerCase().startsWith(searchTerm)
+    })
+    console.log('click', filtroCancion)
+
+    contenido.innerHTML = ''
+    
+    filtroCancion.forEach((song) => {
+        cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
+        cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+        let artists = ''
+        let size = song.trackMetadata.artists.length
+        song.trackMetadata.artists.forEach((item, index) =>{
+            
+            if(index === size - 1){ 
+            artists += item.name
+        }
+            else{
+                artists += item.name + ' / '
+            } 
+        })
+        cardTop.querySelector('.artistname').textContent = artists
+
+        const clone = cardTop.cloneNode(true)
+        fragment.appendChild(clone)
+    });
+    contenido.appendChild(fragment)
+  });
