a3fa85e4344fb628ce3a66e3b1b14c44675e6814
Arath Morales <arath020310@gmail.com>
2023-03-12 18:33:50 -0600

    Canciones de los paises

--- JS/index.js
+++ JS/index.js
@@ -2,18 +2,164 @@ const cardTop = document.querySelector('#cardTop').content
 const contenido = document.querySelector('#contenido')
 const fragment = document.createDocumentFragment()
 const btnBuscar = document.getElementById('buscador')
-let topHundred = [];
+const imgMX = document.getElementById('MX')
+const imgUSA = document.getElementById('USA')
+const imgJP = document.getElementById('JP')
+const imgAR = document.getElementById('AR')
+const imgFR = document.getElementById('FR')
+const imgES = document.getElementById('ES')
+
+const inputAlbum = document.getElementById('inputAlbum')
+const btnBuscarAlbum = document.getElementById('btnBuscar')
+let topTwoHundred = []
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadMusicData();
+    loadMusicData()
+})
+
+//Para mostrar las canciones populares de Mexico
+imgMX.addEventListener('click', () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '39151941b2msh98110608fcd8d30p1ea95bjsne3ed49bb7562',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://spotify81.p.rapidapi.com/top_200_tracks?country=MX', options)
+        .then(response => response.json())
+        .then(response => {
+            topTwoHundred = []
+            topTwoHundred = response
+            //Songs = response
+            creaCards(topTwoHundred)
+            console.log('canciones', topTwoHundred)
+        })
+        .catch(err => console.error(err));
 })
 
 
+//Para mostrar las canciones populares de USA
+imgUSA.addEventListener('click', () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '39151941b2msh98110608fcd8d30p1ea95bjsne3ed49bb7562',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://spotify81.p.rapidapi.com/top_200_tracks?country=US', options)
+        .then(response => response.json())
+        .then(response => {
+            topTwoHundred = []
+            topTwoHundred = response
+            //Songs = response
+            creaCards(topTwoHundred)
+            console.log('canciones', topTwoHundred)
+        })
+        .catch(err => console.error(err));
+})
+
+
+//Para mostrar las canciones populares de japon
+imgJP.addEventListener('click', () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '39151941b2msh98110608fcd8d30p1ea95bjsne3ed49bb7562',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://spotify81.p.rapidapi.com/top_200_tracks?country=JP', options)
+        .then(response => response.json())
+        .then(response => {
+            topTwoHundred = []
+            topTwoHundred = response
+            //Songs = response
+            creaCards(topTwoHundred)
+            console.log('canciones', topTwoHundred)
+        })
+        .catch(err => console.error(err));
+})
+
+
+//Para mostrar las canciones populares de argentina
+imgAR.addEventListener('click', () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '39151941b2msh98110608fcd8d30p1ea95bjsne3ed49bb7562',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://spotify81.p.rapidapi.com/top_200_tracks?country=AR', options)
+        .then(response => response.json())
+        .then(response => {
+            topTwoHundred = []
+            topTwoHundred = response
+            //Songs = response
+            creaCards(topTwoHundred)
+            console.log('canciones', topTwoHundred)
+        })
+        .catch(err => console.error(err));
+})
+
+
+//Para mostrar las canciones populares de francia
+imgFR.addEventListener('click', () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '39151941b2msh98110608fcd8d30p1ea95bjsne3ed49bb7562',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://spotify81.p.rapidapi.com/top_200_tracks?country=FR', options)
+        .then(response => response.json())
+        .then(response => {
+            topTwoHundred = []
+            topTwoHundred = response
+            //Songs = response
+            creaCards(topTwoHundred)
+            console.log('canciones', topTwoHundred)
+        })
+        .catch(err => console.error(err));
+})
+
+
+//Para mostrar las canciones populares de españa
+imgES.addEventListener('click', () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '39151941b2msh98110608fcd8d30p1ea95bjsne3ed49bb7562',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://spotify81.p.rapidapi.com/top_200_tracks?country=ES', options)
+        .then(response => response.json())
+        .then(response => {
+            topTwoHundred = []
+            topTwoHundred = response
+            creaCards(topTwoHundred)
+            console.log('canciones', topTwoHundred)
+        })
+        .catch(err => console.error(err));
+})
+
+
+//Trae las canciones de la pagina https://rapidapi.com/airaudoeduardo/api/spotify81/
 const loadMusicData = () => {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '4d425b0d96mshbed6647e5305b91p14d902jsn73e2a1a73e94',
+            'X-RapidAPI-Key': '39151941b2msh98110608fcd8d30p1ea95bjsne3ed49bb7562',
             'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
         }
     };
@@ -21,77 +167,61 @@ const loadMusicData = () => {
     fetch('https://spotify81.p.rapidapi.com/top_200_tracks', options)
         .then(response => response.json())
         .then(response => {
-            topHundred = response
-            console.log('Canciones: ', topHundred)
-            creaCards()
+            topTwoHundred = response
+            //Songs = response
+            creaCards(topTwoHundred)
+            console.log('canciones', topTwoHundred)
         })
         .catch(err => console.error(err));
 }
 
-const creaCards = () => {
-    topHundred.forEach((song) => {
+const creaCards = (musica) => {
+    contenido.innerHTML = ""
+    musica.forEach((song) => {
         cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
         cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+        
+        // Extraer solo el nombre del todo arreglo de artists
         let artists = ''
-        let size = song.trackMetadata.artists.length
-        song.trackMetadata.artists.forEach((item, index) =>{
-            
-            if(index === size - 1){ 
-            artists += item.name
-        }
-            else{
+        let size = song.trackMetadata.artists.length //extrae el total de nombres
+        song.trackMetadata.artists.forEach((item, index) => {
+            //console.log(index, size)
+            if (index == size-1){ //cuando llega al último nombre ya no le agrega la /
+                artists += item.name
+            } else {
                 artists += item.name + ' / '
-            } 
+            }
         })
         cardTop.querySelector('.artistname').textContent = artists
-
+        
+        //Clonar
         const clone = cardTop.cloneNode(true)
         fragment.appendChild(clone)
-    });
+    })
     contenido.appendChild(fragment)
 }
 
-/*btnBuscar.addEventListener('keyup', () => {
-    console.log('Tecla: ',btnBuscar.value)
-    var array = []
-    for(const obj of topHundred){
-        array.push(obj.trackMetadata.trackName)
-    }
-
-    let filtrado = array.includes(btnBuscar.value.toLowerCase() )
-    //const BuscarCancion = topHundred.trackMetadata.filter((cancion) = cancion.trackName == btnBuscar.value)
-    
-    console.log('Prueba', filtrado)
-})*/
-
-
+//Función para buscar una canción 
 btnBuscar.addEventListener('keyup', () => {
-    const searchTerm = btnBuscar.value.toLowerCase()
-    const filtroCancion = topHundred.filter((song) => {
-      return song.trackMetadata.trackName.toLowerCase().startsWith(searchTerm)
-    })
-    console.log('click', filtroCancion)
+    let temp = []
+    temp = topTwoHundred.filter(song => song.trackMetadata.trackName.toLowerCase().includes(btnBuscar.value.toLowerCase()))
+        //song.trackMetadata.trackName.includes(btnBuscar.value)
+        //song.trackMetadata.trackName.toLowerCase().includes(btnBuscar.value.toLowerCase())
+    creaCards(temp)
+}) 
 
-    contenido.innerHTML = ''
-    
-    filtroCancion.forEach((song) => {
-        cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
-        cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
-        let artists = ''
-        let size = song.trackMetadata.artists.length
-        song.trackMetadata.artists.forEach((item, index) =>{
-            
-            if(index === size - 1){ 
-            artists += item.name
+btnBuscarAlbum.addEventListener('click', () => {
+    const request = inputAlbum.value.toLowerCase()
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '39151941b2msh98110608fcd8d30p1ea95bjsne3ed49bb7562',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
         }
-            else{
-                artists += item.name + ' / '
-            } 
-        })
-        cardTop.querySelector('.artistname').textContent = artists
-
-        const clone = cardTop.cloneNode(true)
-        fragment.appendChild(clone)
-    });
-    contenido.appendChild(fragment)
-  });
+    };
+    
+    fetch(`https://spotify81.p.rapidapi.com/search?q=${request}&type=multi&offset=0&limit=10&numberOfTopResults=5`, options)
+        .then(response => response.json())
+        .then(response => console.log(response.albums.items)) //Solo para mostrar dentro del objeto los albus e items
+        .catch(err => console.error(err));
+})
